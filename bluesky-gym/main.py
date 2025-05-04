052e2160ad01954df358cd6efc96b1f0146a5c3b
JOSHUA SILVA <joshuasilva414@gmail.com>
2025-05-04 18:31:50 +0000

    buffer

--- bluesky-gym/main.py
+++ bluesky-gym/main.py
@@ -1,16 +1,11 @@
 import gymnasium as gym
 import bluesky_gym
-
+from stable_baselines3 import DDPG
 bluesky_gym.register_envs()
 
-env = gym.make('MergeEnv-v0', render_mode='human')
+# env = gym.make('MergeEnv-v0', render_mode='human')
+env = gym.make('MergeEnv-v0')
 
-obs, info = env.reset()
-done = truncated = False
-for _ in range(10):
-    action = env.action_space.sample()
-    obs, reward, done, truncated, info = env.step(action)
-    print(info)
-    env.render()
-
-env.close()
+model = DDPG("MultiInputPolicy", env)
+model.learn(total_timesteps=2e3)
+model.save(path="ddpg_test")
